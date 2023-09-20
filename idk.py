8e779f54981cfc1036b1f2a9b70f020c1a1915b9
Bajkinman <mikaelhagl@gmail.com>
2023-09-20 22:21:58 +0200

    initial commit

---
+++ idk.py
@@ -0,0 +1,8 @@
+# want a class: knight that has a method move(dir) which updates the pos of the
+# knight which i guess is another method that just returns the current position of the knight.
+
+
+
+
+
+
