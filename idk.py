b2d5a48cc1e1591082f9b7c463e9c7f962c16743
Bajkinman <mikaelhagl@gmail.com>
2023-09-21 10:17:41 +0200

    add legalmoves method and update move method

--- idk.py
+++ idk.py
@@ -10,72 +10,44 @@ class Knight:
     def __init__(self, initialpos):
         self.pos = initialpos
 
-    def move(self,direction): #direction should be an integer between 0 and 7
+    def legalmoves(self):
+        legal = set()
+        for i in range(8):
+            ex = ey = e = 0
+            if i in {2,3,4,5}:
+                ex = 1
+            if i in {4,5,6,7}:
+                ey = 1
+            if i in {1,2,5,6}:
+                e = 1
+
+            x,y = int(2**(1-e)*(-1)**ex),int(2**e*(-1)**ey)
+            
+            if 0 <= self.pos[0]+x <= 7 and 0 <= self.pos[1]+y <= 7:
+                legal.add(i)
+        return legal
+
+    def move(self,direction):
+        if direction not in self.legalmoves():
+            print("you are bad")
+            return
         
-        if direction == 0: # >>^
-            if self.pos[0]+2 >= 8 or self.pos[1]+1 >= 8:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]+2,self.pos[1]+1)
-        
-        elif direction == 1: # ^^>
-            if self.pos[0]+1 >= 8 or self.pos[1]+2 >= 8:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]+1,self.pos[1]+2)
-        
-        elif direction == 2: # ^^<
-            if self.pos[0]-1 < 0 or self.pos[1]+2 >= 8:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]-1,self.pos[1]+2)
-        
-        elif direction == 3: # <<^
-            if self.pos[0]-2 < 0 or self.pos[1]+1 >= 8:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]-2,self.pos[1]+1)
-        
-        elif direction == 4: # <<v
-            if self.pos[0]-2 < 0 or self.pos[1]-1 < 0:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]-2,self.pos[1]-1)
-        
-        elif direction == 5: # vv<
-            if self.pos[0]-1 < 0 or self.pos[1]-2 < 0:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]-1,self.pos[1]-2)
-        
-        elif direction == 6: # vv>
-            if self.pos[0]+1 >= 8 or self.pos[1]-2 < 0:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]+1,self.pos[1]-2)
-        
-        elif direction == 7: # >>v
-            if self.pos[0]+2 >= 8 or self.pos[1] < 0:
-                # invalid
-                pass
-            else:
-                self.pos = (self.pos[0]+2,self.pos[1]-1)
-        else:
-            # do something, say you are bad
-            # return something
-            pass
+        ex = ey = e = 0
+        if direction in {2,3,4,5}:
+            ex = 1
+        if direction in {4,5,6,7}:
+            ey = 1
+        if direction in {1,2,5,6}:
+            e = 1
+
+        x,y = int(2**(1-e)*(-1)**ex),int(2**e*(-1)**ey)
+        print("x,y",x,y)
+        self.pos[0] += x
+        self.pos[1] += y
+
 
 # maybe have a method named validmoves that returns the directions to which the knight legally can jump.
 # think that might be quite nice actually, then i don't have to check all of this bs in the move method
-
-
 
 class Board:
     def __init__(self):
@@ -99,16 +71,15 @@ class Board:
 
 board = Board()
 
-#board.printBoard()
-
-"""
 # testing move stuff
-
-knight = Knight((0,0))
-print(knight.pos)
-
-knight.move(0)
-knight.move(1)
-print(knight.pos)
-"""
+knight = Knight([0,0])
+print("starting pos",knight.pos)
+while True:
+    print("legal moves:", knight.legalmoves())
+    inp = input("Choose a direction for a move ")
+    if inp == "quit":
+        break
+    inp = int(inp)
+    knight.move(inp)
+    print("new position:",knight.pos)
 
